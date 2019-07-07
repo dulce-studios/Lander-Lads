0dbc0531d11de7cb0f4e914101872a22157474b2
theAmbientOrange <therealjameswu@gmail.com>
2019-07-07 12:38:33 -0400

    Add force vectors and curve float fields, initial action binding methods

--- Source/LanderLads/SpaceshipPawn.h
+++ Source/LanderLads/SpaceshipPawn.h
@@ -4,6 +4,7 @@
 
 #include "CoreMinimal.h"
 #include "Components/InputComponent.h"
+#include "Curves/CurveFloat.h"
 #include "GameFramework/Pawn.h"
 #include "SpaceshipStaticMeshComponent.h"
 
@@ -24,11 +25,24 @@ protected:
 
 	// Used for handling inputs
 	virtual void SetupPlayerInputComponent(UInputComponent* InputComponent) override;
-	void MoveForwardBackward(float Scale);
-	void MoveLeftRight(float Scale);
-	void MoveUpDown(float Scale);
-	void MoveCameraLookHorizontal(float Scale);
-	void MoveCameraLookVertical(float Scale);
+	
+	// Axis bindings
+	void MoveForwardBackward(float AxisValue);
+	void MoveLeftRight(float AxisValue);
+	void MoveUpDown(float AxisValue);
+	void MoveCameraLookHorizontal(float AxisValue);
+	void MoveCameraLookVertical(float AxisValue);
+
+	// Action bindings
+	void MoveUpButtonPressed();
+
+	UFUNCTION()
+	void OnCompHit(
+		UPrimitiveComponent* HitComp, 
+		AActor* OtherActor, 
+		UPrimitiveComponent* OtherComp, 
+		FVector NormalImpulse, 
+		const FHitResult& Hit);
 
 	// Getters
 	USpaceshipStaticMeshComponent* GetSpaceshipStaticMeshComponent();
@@ -38,7 +52,16 @@ public:
 	virtual void Tick(float DeltaTime) override;
 
 private:
+	const float ClampAxisValue(float AxisValue);
+
+	UCurveFloat* CurveFloat;
+
 	UPROPERTY(VisibleAnywhere, meta = (AllowPrivateAccess = "true"), BlueprintReadWrite)
 	USpaceshipStaticMeshComponent* SpaceshipStaticMeshComponent;
 
+	float ShipThrustStartTime;
+
+	FVector ForwardBackwardForceVector;
+	FVector UpDownForceVector;
+	FVector LeftRightForceVector;
 };
