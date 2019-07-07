8c54c23b1f7b38c5b804378a5eb6e4bcf01d6c3a
theAmbientOrange <therealjameswu@gmail.com>
2019-07-07 12:39:40 -0400

    Handle vertical thrust movement, clamp axis bindings between -1.0f and 1.0f

--- Source/LanderLads/SpaceshipPawn.cpp
+++ Source/LanderLads/SpaceshipPawn.cpp
@@ -2,6 +2,8 @@
 
 #include "SpaceshipPawn.h"
 
+#include "UObject/ConstructorHelpers.h"
+
 // Sets default values
 ASpaceshipPawn::ASpaceshipPawn()
 {
@@ -15,6 +17,15 @@ ASpaceshipPawn::ASpaceshipPawn()
 	this->SpaceshipStaticMeshComponent->AttachToComponent(
 		this->GetRootComponent(), FAttachmentTransformRules::KeepRelativeTransform);
 
+	this->ForwardBackwardForceVector = FVector(0);
+	this->LeftRightForceVector = FVector(0);
+	this->UpDownForceVector = FVector(0);
+
+	static ConstructorHelpers::FObjectFinder<UCurveFloat> CurveAsset(
+		TEXT("CurveFloat'/Game/Misc/ThrusterJerk.ThrusterJerk'"));
+	this->CurveFloat = CurveAsset.Object;
+
+
 }
 
 // Called when the game starts or when spawned
@@ -22,6 +33,8 @@ void ASpaceshipPawn::BeginPlay()
 {
 	Super::BeginPlay();
 	
+	this->SpaceshipStaticMeshComponent->OnComponentHit.AddDynamic(this, &ASpaceshipPawn::OnCompHit);
+
 }
 
 // Called every frame
@@ -29,18 +42,34 @@ void ASpaceshipPawn::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
 
+	FVector ResultantForceVector =
+		this->ForwardBackwardForceVector + this->LeftRightForceVector + this->UpDownForceVector;
+
+	this->GetSpaceshipStaticMeshComponent()->AddForce(ResultantForceVector);
 }
 
 // Called to bind functionality to input
 void ASpaceshipPawn::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
 {
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
 	check(PlayerInputComponent);
+
+	PlayerInputComponent->BindAction("MoveUpButtonPressed", EInputEvent::IE_Pressed, this, &ASpaceshipPawn::MoveUpButtonPressed);
 
 	PlayerInputComponent->BindAxis("ForwardBackward", this, &ASpaceshipPawn::MoveForwardBackward);
 	PlayerInputComponent->BindAxis("LeftRight", this, &ASpaceshipPawn::MoveLeftRight);
 	PlayerInputComponent->BindAxis("UpDown", this, &ASpaceshipPawn::MoveUpDown);
 	PlayerInputComponent->BindAxis("CameraLookHorizontal", this, &ASpaceshipPawn::MoveCameraLookHorizontal);
 	PlayerInputComponent->BindAxis("CameraLookVertical", this, &ASpaceshipPawn::MoveCameraLookVertical);
+}
+
+void ASpaceshipPawn::MoveUpButtonPressed()
+{
+	UWorld* World = this->GetWorld();
+	if (World) {
+		this->ShipThrustStartTime = World->GetRealTimeSeconds();
+	}
 }
 
 void ASpaceshipPawn::MoveForwardBackward(float AxisValue)
@@ -55,22 +84,49 @@ void ASpaceshipPawn::MoveLeftRight(float AxisValue)
 
 void ASpaceshipPawn::MoveUpDown(float AxisValue)
 {
-
+	UWorld* World = this->GetWorld();
+	if (World) {
+		const float CounterGravity = World->GetGravityZ() * -1;
+		const float ClampedAxisValue = ClampAxisValue(AxisValue);
+		const float TimeDifference = World->GetRealTimeSeconds() - this->ShipThrustStartTime;
+		const float Curvey = this->CurveFloat->GetFloatValue(TimeDifference);
+		float UpDownForceVectorMagnitude = CounterGravity * 4000 * ClampedAxisValue * Curvey;
+		this->UpDownForceVector.SetComponentForAxis(EAxis::Z, UpDownForceVectorMagnitude);
+	}
 }
 
 void ASpaceshipPawn::MoveCameraLookHorizontal(float AxisValue)
 {
+	float ClampedAxisValue = ClampAxisValue(AxisValue);
 	this->AddControllerYawInput(
-		this->GetWorld()->GetDeltaSeconds() * 45 * AxisValue);
+		this->GetWorld()->GetDeltaSeconds() * 45 * ClampedAxisValue);
 }
 
 void ASpaceshipPawn::MoveCameraLookVertical(float AxisValue)
 {
+	float ClampedAxisValue = ClampAxisValue(AxisValue);
 	this->AddControllerPitchInput(
-		this->GetWorld()->GetDeltaSeconds() * -45 * AxisValue);
+		this->GetWorld()->GetDeltaSeconds() * -45 * ClampedAxisValue);
+}
+
+const float ASpaceshipPawn::ClampAxisValue(float AxisValue)
+{
+	return FMath::Clamp(AxisValue, -1.0f, 1.0f);
 }
 
 USpaceshipStaticMeshComponent* ASpaceshipPawn::GetSpaceshipStaticMeshComponent()
 {
 	return this->SpaceshipStaticMeshComponent;
+}
+
+void ASpaceshipPawn::OnCompHit(
+	UPrimitiveComponent* HitComp,
+	AActor* OtherActor,
+	UPrimitiveComponent* OtherComp,
+	FVector NormalImpulse,
+	const FHitResult& Hit)
+{
+	if (HitComp == this->GetSpaceshipStaticMeshComponent()) {
+		UE_LOG(LogTemp, Warning, TEXT("WE ARE HIT!!"));
+	}
 }
